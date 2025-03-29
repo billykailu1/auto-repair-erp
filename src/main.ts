a7ddf4b88f0611449f247cf7f7a5df09845474ac
Donovan Hodges <dghodges@umich.edu>
2025-03-29 17:50:08 -0400

    Made changes to main.ts to enable running the front and backend at the same time

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS with a specific origin
+  app.enableCors({
+    origin: 'http://localhost:3000', // Specify the allowed origin (your Next.js app's address)
+    methods: 'GET, POST, PUT, DELETE', // Allowed methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  });
+
+  await app.listen(3001); // Port your NestJS server is listening on
 }
 bootstrap();
