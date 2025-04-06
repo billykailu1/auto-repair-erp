c94e0cd0392d67305ed495544df47b92c75ce32e
Donovan Hodges <dghodges@umich.edu>
2025-04-06 16:45:52 -0400

    Added AlertsModule to app module to use throughout

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppointmentsModule } from './appointments/appointments.module';
 import { InventoryModule } from './inventory/inventory.module'; // 🆕
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module'; // Import the UserModule
+import {AlertsModule} from './alerts/alerts.module'; // Import the Alerts module
 
 @Module({
-  imports: [AppointmentsModule, InventoryModule, PrismaModule, UserModule], // 🆕 add InventoryModule here
+  imports: [AppointmentsModule, InventoryModule, PrismaModule, UserModule, AlertsModule], // 🆕 add InventoryModule here
   controllers: [AppController],
   providers: [AppService],
 })
